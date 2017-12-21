e9dac82ab74f88c72e9eb6ba47fbe7b40d4c3cb5
Raghu Kaippully <raghu.kaippully@concur.com>
2017-12-21 20:46:45 +0530

    Day3 Part2

--- src/Advent/Day3.hs
+++ src/Advent/Day3.hs
@@ -1,23 +1,51 @@
-module Advent.Day3 where
+module Advent.Day3 (day3part1, day3part2) where
 
 import           Advent.Types (Problem(Problem))
 import qualified Data.ByteString.Lazy.Char8 as BS
 
-data Direction = Asc | Desc deriving (Eq)
+data Side = SRight | STop | SLeft | SBottom
+  deriving (Eq)
 
-manhattanDistances :: [Int]
-manhattanDistances = 0 : concatMap gen [1..]
+type Coord = (Int, Int)
+
+coordinates :: [Coord]
+coordinates = (0, 0) : gen SRight (1, 0) 1
   where
-    -- generate the manhattan distance list for `lvl`
-    gen :: Int -> [Int]
-    gen lvl = take (lvl * 8) $ cycle $ [lvl*2-1, lvl*2-2..lvl] ++ [lvl+1..lvl*2]
+    gen :: Side -> Coord -> Int -> [Coord]
+    gen SRight v@(x, y) lvl  | y == lvl  = v : gen STop (x - 1, y) lvl
+                             | otherwise = v : gen SRight (x, y + 1) lvl
+    gen STop v@(x, y) lvl    | x == negate lvl = v : gen SLeft (x, y - 1) lvl
+                             | otherwise       = v : gen STop (x - 1, y) lvl
+    gen SLeft v@(x, y) lvl   | y == negate lvl = v : gen SBottom (x + 1, y) lvl
+                             | otherwise       = v : gen SLeft (x, y - 1) lvl
+    gen SBottom v@(x, y) lvl | x == lvl  = v : gen SRight (x + 1, y) (lvl + 1)
+                             | otherwise = v : gen SBottom (x + 1, y) lvl
+
+manhattanDistance :: Coord -> Coord -> Int
+manhattanDistance (x1, y1) (x2, y2) = abs (x2 - x1) + abs (y2 - y1)
 
 day3part1 :: Problem
 day3part1 = Problem "day3part1" $ \s ->
   let
     n = read $ BS.unpack s
   in
-    BS.pack $ show $ manhattanDistances!!(n - 1)
+    BS.pack $ show $ manhattanDistance (coordinates!!(n - 1)) (0, 0)
+
+sums :: [Int]
+sums = 1 : genSums 1
+  where
+    genSums :: Int -> [Int]
+    genSums idx = sumOf idx : genSums (idx + 1)
+
+    sumOf :: Int -> Int
+    sumOf idx = sum $ map snd $ filter (isAdjacent (coordinates!!idx)) $ zip (take idx coordinates) sums
+
+    isAdjacent :: Coord -> (Coord, a) -> Bool
+    isAdjacent (x1, y1) ((x2, y2), _) = abs (x2 - x1) <= 1 && abs (y2 - y1) <= 1
 
 day3part2 :: Problem
-day3part2 = Problem "day3part2" undefined
+day3part2 = Problem "day3part2" $ \s ->
+  let
+    n = read $ BS.unpack s
+  in
+    BS.pack $ show $ head $ dropWhile (<= n) sums
